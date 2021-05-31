bfd51a37d42eb889cdae16c24f6de38772c4fba7
Patrick Schlarmann <patrick.schlarmann@sdx-ag.de>
2021-05-31 16:40:33 +0200

    Basic example

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Channels
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = Channel.CreateUnbounded<int>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                await channel.Writer.WriteAsync(i);
+            }
+
+            while (true)
+            {
+                var item = await channel.Reader.ReadAsync();
+
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
