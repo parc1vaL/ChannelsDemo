ba2c62bd3b7375b4d3b13b8fd9cd176627963a3a
Patrick Schlarmann <patrick.schlarmann@sdx-ag.de>
2021-05-31 17:13:07 +0200

    Back pressure

--- Program.cs
+++ Program.cs
@@ -9,23 +9,38 @@ namespace Channels
     {
         static async Task Main(string[] args)
         {
-            var channel = Channel.CreateUnbounded<int>();
-
-            Task.Run(async () =>
+            var channelOptions = new BoundedChannelOptions(5)
             {
-                for (int i = 1; i <= 10; i++)
-                {
-                    await channel.Writer.WriteAsync(i);
-                    await Task.Delay(1000);
-                }
+                FullMode = BoundedChannelFullMode.Wait, // Default-Verhalten: Warten, bis Kapazität frei ist
+                // FullMode = BoundedChannelFullMode.DropNewest, // Entferne das neueste (hinterste) Element des Channels, um Platz für das neu hinzu zu fügende Element zu schaffen
+                // FullMode = BoundedChannelFullMode.DropOldest, // Entferne das älteste (vorderste) Element des Channels, um Platz für das neu hinzu zu fügende Element zu schaffen
+                // FullMode = BoundedChannelFullMode.DropWrite, // Ignoriere ("droppe") das neu hinzu kommende Element
+            };
 
-                channel.Writer.Complete();
-            });
+            var channel = Channel.CreateBounded<int>(channelOptions);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                await channel.Writer.WriteAsync(i);
+                Console.WriteLine($"{i} eingefügt.");
+
+                // Weitere Schreib-Operationen:
+                // var success = channel.Writer.TryComplete(); // Wirft keine Exception, wenn der Channel bereits als abgeschlossen markiert ist.
+                // var success = channel.Writer.TryWrite(i); // Synchrones Schreiben - Liefert false, falls keine Kapazität frei ist (falls BoundedChannelFullMode.Wait) oder der Channel als abgeschlossen markiert ist
+                // var success = await channel.Writer.WaitToWriteAsync(); // Wartet bis Kapazität frei ist, ohne zu schreiben. Liefert false, falls der Channel vorzeitig als abgeschlossen markiert wird.
+            }
+
+            channel.Writer.Complete();
 
             await foreach (var item in channel.Reader.ReadAllAsync())
             {
-                Console.WriteLine(item);
+                Console.WriteLine($"{item} abgeholt.");
+                await Task.Delay(1000);
             }
+
+            // Weitere Lese-Operationen
+            // var success = channel.Reader.TryRead(out var newItem); // Synchrones Lesen - Liefert false, falls keine Elemente vorhanden sind oder der Channel als abgeschlossen markiert ist
+            // var success = channel.Reader.WaitToReadAsync(); // Wartet, bis ein Element im Channel zur Verfügung steht. Liefert false, falls der Channel vorzeitig als abgeschlossen markiert wird.
 
             Console.WriteLine("All done!");
         }
